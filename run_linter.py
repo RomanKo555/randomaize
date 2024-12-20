790f679c11702ec1bf8cc3b1e3d28b65276a7677
Roman Teleshun <roman.teleshun@dxc.com>
2024-12-20 16:49:38 +0200

    Fix liner

--- run_linter.py
+++ run_linter.py
@@ -5,7 +5,7 @@ import subprocess
 
 def run_linter():
     print("Running linter...")
-    subprocess.run(["flake8", "."])
+    subprocess.run(["flake8", "main.py"])
 
 if __name__ == "__main__":
     run_linter()
